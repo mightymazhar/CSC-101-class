ca466f34f73907a92cdcbb9d380af641458f4b92
mightymazhar <msyed9784@gmail.com>
2017-06-19 13:11:05 +0600

    Add files via upload

---
+++ Mazhar2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    for(int i = 1; i <= n; i++)
+    {
+        for(int k = 1; k<=n; k++)
+       if (i % 2 == 0)
+       {
+           cout << "#";
+       }
+       else
+       {
+           cout << "*";
+       }
+       cout << endl;
+    }
+
+    return 0;
+}
+
---
+++ Mazhar3.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cin >> n;
+    for(int i = 1; i <= n; i++)
+    {
+        for(int j = 1; j <= n; j++)
+       if (i % 2 == 0 && j % 2 != 0)
+       {
+           cout << "#";
+       }
+       else if ( i %2 == 0 && j % 2 == 0 )
+       {
+           cout << "*";
+       }
+       else if (i % 2 != 0 && j % 2 !=0)
+       {
+           cout << "*";
+       }
+       else
+       {
+           cout << "#";
+       }
+
+    cout << endl;
+
+    }
+
+
+    return 0;
+}
